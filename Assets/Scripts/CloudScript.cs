a00b1bb5c1ee705d0a5718089288c9b42cbba132
Munkittaja <joni.kurki@gmail.com>
2018-03-02 12:51:16 +0200

    added some clouds

---
+++ Assets/Scripts/CloudScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudScript : MonoBehaviour {
+
+    GameObject player;
+    public float speed;
+    SpriteRenderer render;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        render = GetComponent<SpriteRenderer>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 offset = new Vector3(0, 0, 0);
+
+        Vector2 scrollOffset = new Vector2(Time.time * speed, 0);
+        GetComponent<Renderer>().material.mainTextureOffset = scrollOffset;
+
+        //transform.position = player.transform.position + offset; 
+	}
+}
